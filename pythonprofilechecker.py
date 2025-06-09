0fc4bfc4270f3742c6b102a38bb29ca6912f7960
Hanin Fawaz <hanin.fawaz.2007@gmail.com>
2025-06-09 17:31:04 +0300

    Update pythonprofilechecker.py

--- pythonprofilechecker.py
+++ pythonprofilechecker.py
@@ -1,9 +1,9 @@
-print("Python profile  checker")
-name= input ("what's your name?:")
+print("python profile checker")
+name= input ("What's your name?:")
 age= int(input("How old your you?:"))
 gpa=float(input("Enter your GPA(0-5):"))
-field=input("what is your field of interset?:")
-graduated=input("Have you graduated?:(yes or no):")
+field=input("What's your field of interset?:")
+graduated=input("Have you graduated?(yes or no):")
 print(name,age,gpa,field,graduated)
 if age<25 and gpa>=3.5 and graduated=="yes":
     print("You are eligible for a scolarship.")
